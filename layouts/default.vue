663a7931db4202379bef6293332509eb4cb13bce
王超凡 <1198920717@qq.com>
2018-01-26 15:02:42 +0800

    修改2

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
+  <div id="default-template">
     <nuxt/>
   </div>
 </template>
 <script>
-import vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
-vue.use(BootstrapVue);
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-export default {
-  layout:'sign',//指明当前页面所使用的默认模板是layouts文件夹下的sign.vue
-}
+  import vue from 'vue'
+  import BootstrapVue from 'bootstrap-vue'
+  vue.use(BootstrapVue);
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
+  export default {
+    layout: 'sign',//指明当前页面所使用的默认模板是layouts文件夹下的sign.vue
+  }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,22 @@
 <!-- 组件的模板 -->
 <template>
     <div>
-    <b-container>
-        <b-row>
-            <b-col cols="9 alert-primary">我占9个</b-col>
-            <b-col cols="3 alert-danger">我占3个</b-col>
-        </b-row>
-    </b-container>
+        <my-header></my-header>
+        <b-container>
+            <b-row>
+                <b-col cols="9">我占9个</b-col>
+                <b-col cols="3">我占3个</b-col>
+            </b-row>
+        </b-container>
     </div>
 </template>
 
 <!-- 组件的模型 -->
 <script>
-export default {
-    // 组件的名称
-    data () {
-        return {
+    import myHeader from '../components/myHeader.vue'
+    export default {
+        components: {
+            myHeader
         }
     }
-}
 </script>
-
-<!-- 组件的样式 -->
-<style>
-
-</style>
